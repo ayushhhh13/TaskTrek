151edb2dbcfd898f88966fd2bc739c8c79fa20cc
Ayush Agrawal <ayushagrawal1330@gmail.com>
2025-01-21 11:50:07 +0530

    sidebar styled

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,13 +1,22 @@
 "use client";
-import { LockIcon } from 'lucide-react';
+import { useAppDispatch, useAppSelector } from '@/app/redux';
+import { setIsSidebarCollapsed } from '@/state';
+import { Home, Link, LockIcon, LucideIcon, X } from 'lucide-react';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 import React, { useState } from 'react'
 
 const Sidebar = () => {
     const [showProjects, setShowProjects] = useState(true);
     const [showPriority, setShowPriority] = useState(true);
 
-    const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white w-64`;
+    const dispatch = useAppDispatch();
+    const isSidebarCollapsed = useAppSelector(
+            (state) => state.global.isSidebarCollapsed,
+    );
+
+    const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white 
+    ${isSidebarCollapsed ? "w-0 hidden" : "w-64"}`;
 
   return (
     <div className={sidebarClassNames}>
@@ -17,23 +26,78 @@ const Sidebar = () => {
             <div className="text-xl font-bold text-gray-800 dark:text-white">
                 TaskTrek
             </div>
+            {isSidebarCollapsed ? null :(
+              <button 
+                className='py-3' 
+                onClick={() => {
+                  dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
+                }
+              >
+                <X className="h-6 w-6 text-gray-800 hover:text-gray-500 dark:text-white" />
+              </button>
+            )}
         </div>
         {/* TEAM */}
         <div className='flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700 '>
             <Image  src="/logo.png" alt='Logo' width={40} height={40}/>
             <div>
-                <h3 className='text-md font-bold tracking-wide dark:text-grap-200'>
+                <h3 className='text-md font-bold tracking-wide dark:text-gray-200'>
                     Ayush Team
                 </h3>
                 <div className='mt-1 flex items-start gap-2'>
                     <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400"/>
-                    <p className='text-xs text-gray-500'>Private </p>
+                    <p className='text-xs text-gray-500'>Private</p>
                 </div>
             </div>
         </div>
         {/* NAVBAR Links */}
+        <nav className='z-10 w-full'>
+          <SidebarLink icon={Home} label="Home" href="/" />
+        </nav>
       </div>
     </div>
+  )
+}
+
+interface SidebarLinkProps{
+  href: string;
+  icon: LucideIcon;
+  label: string;
+  // isCollapsed: boolean;
+}
+
+const SidebarLink =({
+  href,
+  icon: Icon,
+  label,
+  // isCollapsed
+}: SidebarLinkProps) => {
+  const pathname = usePathname();
+  const isActive = ((pathname === href) || (pathname === "/" && href === "/dashboard"));
+  const screenWidth = window.innerWidth;
+
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+          (state) => state.global.isSidebarCollapsed,
+  );
+
+  return (
+    <Link href={href} className='w-full'>
+      <div
+        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 
+          ${ isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""
+       } justify-start px-8 py-3`} 
+      >
+        {isActive && (
+           <div className='absolute left-0 top-0  h-[100%] w-[5px] bg-blue-200'/>
+        )}
+
+        <Icon className='h-6 w-6 text-gray-800 dark:text-gray-100'/>
+        <span className={`font-medium text-gray-800 dark:text-gray-100`}>
+          {label}
+        </span>
+      </div>
+    </Link>
   )
 }
 
