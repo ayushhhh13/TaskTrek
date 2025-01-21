67d2d46c54e1a4a6340bad94a77fccb38e88a8ed
ayushhhh13 <authenticayush13@gmail.com>
2025-01-21 12:06:02 +0530

    5th commit

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -9,12 +9,11 @@ import React, { useState } from 'react'
 const Sidebar = () => {
     const [showProjects, setShowProjects] = useState(true);
     const [showPriority, setShowPriority] = useState(true);
-    
+
     const dispatch = useAppDispatch();
     const isSidebarCollapsed = useAppSelector(
             (state) => state.global.isSidebarCollapsed,
     );
-
     const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white 
     ${isSidebarCollapsed ? "w-0 hidden" : "w-64"}`;
 
